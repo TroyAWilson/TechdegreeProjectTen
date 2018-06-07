e2140b68393771a07607917651f0ca617549b0dc
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-06-07 12:43:54 -0700

    Finished: Modal Styles
    Currently: figuring out how to get the Modals to dispaly the correct information depending on which employee is clicked
    
    Notes: none

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,13 @@
   dataType: 'json',
   success: function(data) {
     for(let i = 0; i < data.results.length; i++){
-    employee += "<div class ='employee-div'><a href ='#''>";
+    employee += "<div class ='employee-div'>";
     employee += '<img class = "employee-image" src ='+data.results[i].picture.medium +' >';
     employee += '<div class ="information">'
     employee += '<p class ="employee-name">' + data.results[i].name.first + ' ' + data.results[i].name.last +'</p>';
     employee += '<p class ="employee-email">'+ data.results[i].email +'</p>';
     employee += '<p class ="employee-location">' + data.results[i].location.city + '</p>';
-    employee += '</a></div>';
+    employee += '</div>';
     employee += '</div>';
 
   };
@@ -19,12 +19,14 @@
   $('.employee-div').click(function(){
     vex.defaultOptions.className = 'vex-theme-flat-attack'
     vex.dialog.open({
-      input: ['<img class = "modal-image" src ='+data.results[0].picture.large +' >'
-      + '<p class ="modal-email">'+ data.results[0].email +'</p>'
-      + '<p class ="modal-city">' + data.results[0].location.city + '</p>'
-      + '<p class ="modal-phone">' + data.results[0].cell + '</p>'
-      + '<p class ="modal-address">' + data.results[0].location.street +' '+ data.results[0].nat +' ' + data.results[0].location.postcode + '</p>'
-      + '<p class ="modal-bday"> Birthdate: ' + data.results[0].dob + '</p>',
+      input: [
+        '<img class = "modal-image" src ='+data.results[0].picture.large +' >'
+      + '<p class ="modal-name">' + data.results[0].name.first + ' ' + data.results[0].name.last +'</p>'
+      + '<p class ="modal-email" style= "color:#83898D">'+ data.results[0].email +'</p>'
+      + '<p class ="modal-city" style= "color:#83898D">' + data.results[0].location.city + '</p>'
+      + '<p class ="modal-phone" style= "color:#83898D">' + data.results[0].cell + '</p>'
+      + '<p class ="modal-address" style= "color:#83898D">' + data.results[0].location.street +' '+ data.results[0].nat +' ' + data.results[0].location.postcode + '</p>'
+      + '<p class ="modal-bday" style= "color:#83898D"> Birthdate: ' + data.results[0].dob + '</p>',
     ].join(''),
     })
   });
