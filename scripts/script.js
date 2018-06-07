be1a061d9ca183f6556efc5e905b3819405dfbb8
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-06-07 13:54:05 -0700

    Finished: Modals now match their respective employees
    Currently: Making media queries for mobile devices
    
    Notes: There was nothing specific about a hover design so I just made something up

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@
   dataType: 'json',
   success: function(data) {
     for(let i = 0; i < data.results.length; i++){
-    employee += "<div class ='employee-div'>";
+    employee += "<div class ='employee-div employee"+ i +"'>";
     employee += '<img class = "employee-image" src ='+data.results[i].picture.medium +' >';
     employee += '<div class ="information">'
     employee += '<p class ="employee-name">' + data.results[i].name.first + ' ' + data.results[i].name.last +'</p>';
@@ -16,20 +16,22 @@
 
   };
   $('#main-container').html(employee);
-  $('.employee-div').click(function(){
+  for(let i = 0; i < data.results.length; i++){
+  $('.employee'+i).click(function(){
     vex.defaultOptions.className = 'vex-theme-flat-attack'
     vex.dialog.open({
       input: [
-        '<img class = "modal-image" src ='+data.results[0].picture.large +' >'
-      + '<p class ="modal-name">' + data.results[0].name.first + ' ' + data.results[0].name.last +'</p>'
-      + '<p class ="modal-email" style= "color:#83898D">'+ data.results[0].email +'</p>'
-      + '<p class ="modal-city" style= "color:#83898D">' + data.results[0].location.city + '</p>'
-      + '<p class ="modal-phone" style= "color:#83898D">' + data.results[0].cell + '</p>'
-      + '<p class ="modal-address" style= "color:#83898D">' + data.results[0].location.street +' '+ data.results[0].nat +' ' + data.results[0].location.postcode + '</p>'
-      + '<p class ="modal-bday" style= "color:#83898D"> Birthdate: ' + data.results[0].dob + '</p>',
-    ].join(''),
+        '<img class = "modal-image" src ='+data.results[i].picture.large +' >'
+      + '<p class ="modal-name">' + data.results[i].name.first + ' ' + data.results[i].name.last +'</p>'
+      + '<p class ="modal-email" style= "color:#83898D">'+ data.results[i].email +'</p>'
+      + '<p class ="modal-city" style= "color:#83898D">' + data.results[i].location.city + '</p>'
+      + '<p class ="modal-phone" style= "color:#83898D">' + data.results[i].cell + '</p>'
+      + '<p class ="modal-address" style= "color:#83898D">' + data.results[i].location.street +' '+ data.results[0].nat +' ' + data.results[0].location.postcode + '</p>'
+      + '<p class ="modal-bday" style= "color:#83898D"> Birthdate: ' + data.results[i].dob + '</p>',
+].join(''),
     })
   });
+}
     console.log(data);
   }
 });
